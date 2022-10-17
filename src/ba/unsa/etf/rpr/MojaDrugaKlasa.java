02e08876049bf2a084638ba035872eb10de6a364
nejrastr <86197301+nejrastr@users.noreply.github.com>
2022-10-17 16:28:17 +0200

    prva verzija

---
+++ src/ba/unsa/etf/rpr/MojaDrugaKlasa.java
@@ -0,0 +1,20 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Scanner;
+
+public class MojaDrugaKlasa {
+    public static void main(String[] args) {
+
+        int x,y;
+
+        Scanner ucitaj=new Scanner(System.in);
+        System.out.println("Unesite prvi broj: ");
+        x=ucitaj.nextInt();
+        System.out.println("Unesite drugi broj: ");
+        y=ucitaj.nextInt();
+        System.out.println("Brojevi glase: ");
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+}
